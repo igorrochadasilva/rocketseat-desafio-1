897f6823a6bf6fccc5c74750e97ecb744a70cd22
igorsilva <igor.silva@performait.com>
2022-03-23 21:51:17 -0300

    challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,15 +16,36 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    
+    //somente se titulo não for vazio
+    if(newTaskTitle){
+    
+      const newTask = {
+        id: Math.floor(100000 + Math.random() * 900000),
+        title: newTaskTitle,
+        isComplete: false
+      }
+    
+      setTasks([...tasks, newTask])   
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID          
+    const updatedTasks = tasks.map(task => {
+      task.id === id && (task.isComplete = !task.isComplete)
+      return task
+    })
+    
+    setTasks(updatedTasks)
   }
 
-  function handleRemoveTask(id: number) {
+  function handleRemoveTask(id: number) {    
     // Remova uma task da listagem pelo ID
-  }
+    const removedTasks = tasks.filter(task => task.id !== id);
+    
+    setTasks(removedTasks);    
+  }  
 
   return (
     <section className="task-list container">
